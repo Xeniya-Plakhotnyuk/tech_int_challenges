f440bfd2c0514ae1d67176b8d22a0ee228c231f8
XeniyaZ <kseniya10081989@gmail.com>
2023-10-11 09:25:53 -0500

    object info getting with map method

--- scrimba.js
+++ scrimba.js
@@ -135,24 +135,152 @@ Example output: true
 
 // console.log(sumArray(studentCount));
 
-const gameNightFood = {
-    "🍕 pizza": 3, 
-    "🌮 tacos": 10, 
-    "🥗 salads": 17,
-    "🍝 pasta": 5
-}
+// const gameNightFood = {
+//     "🍕 pizza": 3, 
+//     "🌮 tacos": 10, 
+//     "🥗 salads": 17,
+//     "🍝 pasta": 5
+// }
 
-function findTheWinner(obj){
-let winnerVotes = 0
-let winnerValue = ''
+// function findTheWinner(obj){
+// let winnerVotes = 0
+// let winnerValue = ''
 
-for (let food in obj){
-    if(obj[food] > winnerVotes){
-        winnerVotes = obj[food]
-        winnerValue = food
+// for (let food in obj){
+//     if(obj[food] > winnerVotes){
+//         winnerVotes = obj[food]
+//         winnerValue = food
+//     }
+// }
+// return `The winner is ${winnerValue} with the ${winnerVotes} votes`
+// }
+
+// console.log(findTheWinner(gameNightFood));
+
+
+const userData = [
+    {
+       "name":{
+          "title":"Mr",
+          "first":"Levent",
+          "last":"Busser"
+       },
+       "dob":{
+          "date":"1971-08-21T01:08:00.099Z",
+          "age":51
+       }
+    },
+    {
+       "name":{
+          "title":"Mr",
+          "first":"Kornelius",
+          "last":"Hamnes"
+       },
+       "dob":{
+          "date":"1961-09-23T13:13:49.283Z",
+          "age":61
+       }
+    },
+    {
+       "name":{
+          "title":"Mademoiselle",
+          "first":"Ute",
+          "last":"Henry"
+       },
+       "dob":{
+          "date":"1956-06-30T11:33:42.549Z",
+          "age":66
+       }
+    },
+    {
+       "name":{
+          "title":"Mr",
+          "first":"Estéfano",
+          "last":"Monteiro"
+       },
+       "dob":{
+          "date":"1945-07-16T19:48:22.796Z",
+          "age":77
+       }
+    },
+    {
+       "name":{
+          "title":"Mr",
+          "first":"Oğuzhan",
+          "last":"Beşerler"
+       },
+       "dob":{
+          "date":"1947-09-28T10:12:30.102Z",
+          "age":75
+       }
+    },
+    {
+       "name":{
+          "title":"Mrs",
+          "first":"Susanna",
+          "last":"Burke"
+       },
+       "dob":{
+          "date":"1961-06-13T21:41:24.455Z",
+          "age":61
+       }
+    },
+    {
+       "name":{
+          "title":"Mrs",
+          "first":"Haritya",
+          "last":"Starickiy"
+       },
+       "dob":{
+          "date":"1945-12-14T21:29:22.822Z",
+          "age":76
+       }
+    },
+    {
+       "name":{
+          "title":"Miss",
+          "first":"Nadja",
+          "last":"Branković"
+       },
+       "dob":{
+          "date":"1993-05-24T11:22:50.967Z",
+          "age":29
+       }
+    },
+    {
+       "name":{
+          "title":"Ms",
+          "first":"Sonja",
+          "last":"Lenzen"
+       },
+       "dob":{
+          "date":"1945-03-21T04:33:04.759Z",
+          "age":77
+       }
+    },
+    {
+       "name":{
+          "title":"Ms",
+          "first":"Shubhangi",
+          "last":"Chatterjee"
+       },
+       "dob":{
+          "date":"1956-12-25T18:50:29.484Z",
+          "age":65
+       }
     }
-}
-return winnerValue
+ ]
+
+ function transformData(data){
+    // use map to loop through the data
+    return data.map(({name, dob})=> {
+              
+        return {
+            fullName: `${name.first} ${name.last}` ,
+            birthday: new Date(dob.date).toDateString()
+        }
+      
+    })
 }
 
-console.log(findTheWinner(gameNightFood));
+console.log(transformData(userData));
